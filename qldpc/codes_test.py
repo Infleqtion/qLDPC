2d7d1ed3b5a5ae18002936f32779e5e86ae4c571
Michael A. Perlin <mika.perlin@gmail.com>
2024-01-15 00:34:33 +0800

    more coverage

--- qldpc/codes_test.py
+++ qldpc/codes_test.py
@@ -91,12 +91,17 @@ def test_qubit_code(num_qubits: int = 5, num_checks: int = 3) -> None:
         assert codes.QuditCode.random(num_qubits, num_checks, field=3).num_qubits
 
 
-def test_CSS_code(field: int = 3) -> None:
+def test_CSS_code() -> None:
     """Miscellaneous CSS code tests and coverage."""
     with pytest.raises(ValueError, match="incompatible"):
-        code_x = codes.ClassicalCode.random(3, 2, field)
-        code_z = codes.ClassicalCode.random(4, 2, field)
-        codes.CSSCode(code_x, code_z, field)
+        code_x = codes.ClassicalCode.random(3, 2)
+        code_z = codes.ClassicalCode.random(4, 2)
+        codes.CSSCode(code_x, code_z)
+
+    with pytest.raises(ValueError, match="different fields"):
+        code_x = codes.ClassicalCode.random(3, 2, 2)
+        code_z = codes.ClassicalCode.random(4, 2, 3)
+        codes.CSSCode(code_x, code_z)
 
 
 def test_deformations(num_qudits: int = 5, num_checks: int = 3) -> None:
