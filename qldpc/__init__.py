66213fed2de664e26ba4ca26a36aa33e2af8af0f
Michael A. Perlin <mika.perlin@gmail.com>
2024-01-16 11:12:31 +0800

    import cleanup

--- qldpc/__init__.py
+++ qldpc/__init__.py
@@ -2,16 +2,12 @@ import importlib.metadata
 
 from . import abstract, codes, decoder, objects
 
-decode = decoder.decode
-
 __version__ = importlib.metadata.version("qldpc")
 
 __all__ = [
     "__version__",
     "abstract",
-    "circuits",
     "codes",
-    "decode",
     "decoder",
     "objects",
 ]
