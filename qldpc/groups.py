bc40c290a390fe9fe2b2fe5214ffec64e33859ac
mittaltushant <mittaltushant@gmail.com>
2024-01-18 19:33:18 -0600

    Generate perm cycles for groups
    Generate the perm cycles for groups PSL and SL

--- qldpc/groups.py
+++ qldpc/groups.py
@@ -14,10 +14,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 """
-import warnings
 
-import cvxpy
-import ldpc
 import numpy as np
 import numpy.typing as npt
 import networkx as nx
@@ -78,13 +75,43 @@ def construct_projspace(field:int, dimension:int):
     vectors = itertools.product(gf.elements, repeat=dimension)
     for v in vectors:
         if v[(gf(v)!=0).argmax()] == 1: 
-            proj_space.append(v)
+            proj_space.append(gf(v).data.tobytes())
     return proj_space
 
-def construct_lineargroup(field:int, dimension:int):
+def construct_lineargroup(field:int, dimension:int, proj_space, group):
     """
     Construct a hashdict of the projspace
     multiply these elements by the PSL/SL  matrices and generate the permutation matrices by lookup
     Construct sympy group using these permutation matrices
     """
-    return NotImplemented
+    gf = galois.GF(field)
+    #proj_space = construct_projspace(field, dimension)
+    perm_group = []
+    num = set(range(len(proj_space)))
+    for M in group:
+        num_temp = num.copy()
+        #print(M)
+        perm_string = list(num_temp)
+        while len(num_temp) > 0:
+            index = num_temp.pop() 
+            cycle = [index]
+            current_vector = gf(np.frombuffer(proj_space[index], dtype=np.uint8))
+            #print(current_vector, index)
+            next_index = -1
+            while next_index != index: 
+                next_vector = M @ current_vector
+                next_index = proj_space.index(next_vector.data.tobytes())
+                perm_string[index] = next_index
+                if next_index in num_temp:
+                    num_temp.remove(next_index)
+                    cycle.append(next_index)
+                    #print(next_vector, next_index)
+                current_vector = next_vector
+            perm_string.append(cycle)
+        perm_group.append(perm_string)
+        #print(perm_string)
+    return
+
+proj_space = construct_projspace(2, 3)
+psl, sl = construct_linearmat(2, 3)
+construct_lineargroup(2,3,proj_space,psl)
