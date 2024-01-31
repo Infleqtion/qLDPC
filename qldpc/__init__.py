ac1e96c476569bb570b5b792d91334e7fdb905f2
Michael A. Perlin <mika.perlin@gmail.com>
2024-01-31 15:30:00 -0600

    add circuits.py to __init__

--- qldpc/__init__.py
+++ qldpc/__init__.py
@@ -1,12 +1,13 @@
 import importlib.metadata
 
-from . import abstract, codes, decoder, objects
+from . import abstract, circuits, codes, decoder, objects
 
 __version__ = importlib.metadata.version("qldpc")
 
 __all__ = [
     "__version__",
     "abstract",
+    "circuits",
     "codes",
     "decoder",
     "objects",
