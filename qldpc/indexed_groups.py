3259002e26a92ae3a743b0d7fbc887b7cfe358ee
Michael A. Perlin <mika.perlin@gmail.com>
2024-03-18 16:31:38 -0500

    retrieve group from GAP if possible

--- qldpc/indexed_groups.py
+++ qldpc/indexed_groups.py
@@ -1,4 +1,4 @@
-"""Module for loading indexed groups from GroupNames.org
+"""Module for loading indexed groups from GAP or GroupNames.org
 
    Copyright 2023 The qLDPC Authors and Infleqtion Inc.
 
@@ -16,6 +16,7 @@
 """
 
 import re
+import subprocess
 import urllib.request
 
 import sympy.combinatorics as comb
@@ -23,7 +24,7 @@ import sympy.combinatorics as comb
 from qldpc import abstract
 
 
-def get_group_page(
+def get_GroupNames_url(
     order: int,
     index: int,
     base_url: str = "https://people.maths.bris.ac.uk/~matyd/GroupNames/",
@@ -52,11 +53,11 @@ def get_group_page(
     return base_url + match.group(1)
 
 
-def get_group_generators(order: int, index: int) -> list[abstract.GroupMember]:
+def get_generators_from_GroupNames(order: int, index: int) -> list[abstract.GroupMember]:
     """Get a finite group by its index on GroupNames.org."""
 
     # load web page for the specified group
-    url = get_group_page(order, index)
+    url = get_GroupNames_url(order, index)
     page = urllib.request.urlopen(url)
     html = page.read().decode("utf-8")
 
@@ -88,10 +89,55 @@ def get_group_generators(order: int, index: int) -> list[abstract.GroupMember]:
     return generators
 
 
+def get_generators_with_GAP(order: int, index: int) -> list[abstract.GroupMember]:
+    """Get a finite group from the GAP computer algebra system."""
+    # build GAP command
+    gap_lines = [
+        f"G := SmallGroup({order},{index});",
+        "iso := IsomorphismPermGroup(G);",
+        "permG := Image(iso,G);",
+        "gens := GeneratorsOfGroup(permG);",
+        r'for gen in gens do Print(gen, "\n"); od;',
+        "QUIT;",
+    ]
+    gap_script = "".join(gap_lines)
+
+    # run GAP
+    result = subprocess.run(["gap", "-q", "-c", gap_script], capture_output=True, text=True)
+
+    # collect generators
+    generators = []
+    for line in result.stdout.split("\n")[:-1]:
+        cycles_str = line[1:-1].split(")(")
+
+        try:
+            cycles = [tuple(map(int, cycle.split(","))) for cycle in cycles_str]
+        except ValueError:
+            raise ValueError(f"Cannot extract cycle from string: {line}")
+
+        # decrement integers in the cycle by 1 to account for 0-indexing
+        cycles = [tuple(index - 1 for index in cycle) for cycle in cycles]
+
+        # add generator
+        generators.append(abstract.GroupMember(cycles))
+
+    return generators
+
+
+def is_GAP_installed() -> bool:
+    """Is GAP installed?"""
+    result = subprocess.run(["script", "-c", "gap --version"], capture_output=True, text=True)
+    lines = result.stdout.split("\n")
+    return len(lines) == 2 and lines[1][:3] == "GAP"
+
+
 class IndexedGroup(abstract.Group):
     """Groups indexed on GroupNames.org."""
 
-    def __init__(self, order: int, index: int) -> None:
-        generators = get_group_generators(order, index)
+    def __init__(self, order: int, index: int, with_GAP: bool = True) -> None:
+        if is_GAP_installed():
+            generators = get_generators_with_GAP(order, index)
+        else:
+            generators = get_generators_from_GroupNames(order, index)
         group = comb.PermutationGroup(*generators)
         super().__init__(group)
