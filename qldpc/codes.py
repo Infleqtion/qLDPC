1139e1ea86ec5a1de8cbfda11920ad3d9c9beff9
Michael A. Perlin <mika.perlin@gmail.com>
2024-03-30 17:00:12 -0500

    formatting and type fixes

--- qldpc/codes.py
+++ qldpc/codes.py
@@ -60,12 +60,12 @@ def get_random_array(
     field: type[galois.FieldArray],
     shape: int | tuple[int, ...],
     *,
-    filter: Callable[[npt.NDArray[np.int_]], bool] = lambda _: True,
+    cond: Callable[[galois.FieldArray], bool | np.bool_] = lambda _: True,
     seed: int | None = None,
 ) -> galois.FieldArray:
     """Get a random vector of a given size."""
     seed = get_scrambled_seed(seed) if seed is not None else None
-    while not filter(array := field.Random(shape, seed=seed)):
+    while not cond(array := field.Random(shape, seed=seed)):
         seed = seed + 1 if seed is not None else None  # pragma: no cover
     return array
 
@@ -395,7 +395,7 @@ class ClassicalCode(AbstractCode):
         valid_candidate_found = False
         while not valid_candidate_found:
             # construct an effective check matrix with a random nonzero word
-            random_word = get_random_array(self.field, self.num_bits, filter=lambda vec: vec.any())
+            random_word = get_random_array(self.field, self.num_bits, cond=lambda vec: vec.any())
             effective_check_matrix = np.vstack([self.matrix, random_word]).view(np.ndarray)
 
             # find a low-weight candidate code word
@@ -442,11 +442,11 @@ class ClassicalCode(AbstractCode):
         """
         code_field = galois.GF(field or DEFAULT_FIELD_ORDER)
 
-        def filter(matrix: galois.FieldArray) -> bool:
+        def cond(matrix: galois.FieldArray) -> bool:
             """Return True iff all rows and columns are nonzero."""
             return all(row.any() for row in matrix) and all(col.any() for col in matrix.T)
 
-        matrix = get_random_array(code_field, (checks, bits), filter=filter, seed=seed)
+        matrix = get_random_array(code_field, (checks, bits), cond=cond, seed=seed)
         return ClassicalCode(matrix)
 
     @classmethod
