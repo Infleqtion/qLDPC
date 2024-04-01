7c173ed57731b32b88b1d43969ff75ba28a189e5
Michael A. Perlin <mika.perlin@gmail.com>
2024-03-31 21:44:30 -0500

    update code script

--- experiments/quantum_tanner_search.py
+++ experiments/quantum_tanner_search.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 """Quantum Tanner code search experiment."""
 
+import hashlib
 import itertools
 import os
-from collections.abc import Iterator
+from collections.abc import Hashable, Iterator
 
 from qldpc import abstract, codes
+
+
+def get_deterministic_hash(*inputs: Hashable, bytes: int = 4):
+    """Get a deterministic hash from the given inputs."""
+    input_bytes = repr(inputs).encode("utf-8")
+    hash_bytes = hashlib.sha256(input_bytes).digest()
+    return int.from_bytes(hash_bytes[:bytes], byteorder="big", signed=False)
 
 
 def get_cordaro_wagner_code(length: int) -> codes.ClassicalCode:
@@ -39,16 +47,18 @@ def get_groups(max_order: int = 20) -> Iterator[abstract.SmallGroup]:
 def get_codes_with_tags() -> Iterator[codes.ClassicalCode]:
     """Iterator over several small classical codes and their identifier tags."""
     yield from itertools.chain(
-        ((codes.HammingCode(rr), f"h{rr}") for rr in [2, 3]),
-        ((get_cordaro_wagner_code(nn), f"cw{nn}") for nn in [3, 4, 5, 6]),
-        ((get_mittal_code(nn), f"m{nn}") for nn in [4, 5, 6]),
+        ((codes.HammingCode(rr), f"hamming-{rr}") for rr in [2, 3]),
+        ((get_cordaro_wagner_code(nn), f"CW-{nn}") for nn in [3, 4, 5, 6]),
+        ((get_mittal_code(nn), f"M-{nn}") for nn in [4, 5, 6]),
     )
 
 
 if __name__ == "__main__":
-    save_dir = os.path.join(os.path.dirname(__file__), "quantum_tanner_codes")
     num_samples = 100
     num_trials = 1000
+    save_dir = os.path.join(os.path.dirname(__file__), "quantum_tanner_codes")
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
 
     for group, (base_code, base_tag) in itertools.product(get_groups(), get_codes_with_tags()):
         if group.order < base_code.num_bits:
@@ -56,20 +66,20 @@ if __name__ == "__main__":
             continue
 
         for sample in range(num_samples):
-            seed = hash((group.order, group.index, base_code.matrix.tobytes()))
+            seed = get_deterministic_hash(group.order, group.index, base_code.matrix.tobytes())
             code = codes.QTCode.random(
                 group, base_code, bipartite=False, one_subset=False, seed=seed
             )
-            code_params = code.get_code_params(bound=num_trials)
-            print("Quantum Tanner code parameters:", code_params)
 
+            code_params = code.get_code_params(bound=num_trials)
             headers = [
                 f"group: {group}",
-                f"base code: {base_code}",
+                f"base code: {base_code.name}",
+                f"distance trials: {num_trials}",
                 f"code parameters: {code_params}",
-                f"trial number: {num_trials}",
             ]
 
-            file = f"qt_sg{group.order}-{group.index}_{base_tag}_s{seed}.txt"
+            file = f"qtcode_group-{group.order}-{group.index}_{base_tag}_s{seed}.txt"
             path = os.path.join(save_dir, file)
             code.save(path, *headers)
+            exit()
