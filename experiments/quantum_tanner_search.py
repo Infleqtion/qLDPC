7b24d730785b246dc984f4a39729e83cad89418f
Michael A. Perlin <mika.perlin@gmail.com>
2024-04-01 13:36:12 -0500

    formatting fix

--- experiments/quantum_tanner_search.py
+++ experiments/quantum_tanner_search.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Quantum Tanner code search experiment."""
 
-import hashlib
 import concurrent.futures
+import hashlib
 import os
 import time
 from collections.abc import Hashable, Iterator
