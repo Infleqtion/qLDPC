8ae26c56dbea8ef756c82eec8fead2c707bf7d6d
Michael A. Perlin <mika.perlin@gmail.com>
2024-04-07 11:29:24 -0500

    add comments

--- experiments/quasi_cyclic/make_summary_files.py
+++ experiments/quasi_cyclic/make_summary_files.py
@@ -22,8 +22,8 @@ import numpy as np
 import platformdirs
 from run_search import CACHE_NAME
 
-comm_cutoffs = [5, 8, 10]
-num_trials = 1000
+num_trials = 1000  # number of code distance trials
+comm_cutoffs = [5, 8, 10]  # communication distance "cutoffs" by which to organize results
 
 dirname = os.path.dirname(__file__)
 save_dir = os.path.join(dirname, "codes")
@@ -52,38 +52,48 @@ fmt = "%d, %d, %d, %d, %d, %d, %d, %d, %d, %.3f, %.3f"
 
 ##################################################
 
-data_groups = [[] for _ in range(len(comm_cutoffs))]
-comm_cutoffs = np.array(sorted(comm_cutoffs), dtype=int)
+comm_cutoffs = sorted(comm_cutoffs)
+data_groups: list[list[tuple[int | float, ...]]] = [[] for _ in range(len(comm_cutoffs))]
 
+# iterate over all entries in the cache
 cache = diskcache.Cache(platformdirs.user_cache_dir(CACHE_NAME))
 for key in cache.iterkeys():
     if len(key) != 3:
         continue
 
+    # cyclic group orders and polynomial exponents
     ll, mm, (gg, hh, ii, jj) = key
+
+    # communication distance
     comm = cache[key]
 
-    if comm <= comm_cutoffs[-1]:
-        nn, kk, dd = cache[key + (num_trials,)]
+    if comm > comm_cutoffs[-1]:
+        # we don't care about this code, the communication distance is too large
+        continue
 
-        merit = kk * dd**2 / nn
-        if merit <= 1:
-            continue
+    # code parameters
+    nn, kk, dd = cache[key + (num_trials,)]
 
-        code = (ll, mm, gg, hh, ii, jj, nn, kk, dd, comm, merit)
-        for cutoff, data in zip(comm_cutoffs, data_groups):
-            if comm <= cutoff:
-                data.append(code)
-                break
+    # figure of merit, relative to the surface code
+    merit = kk * dd**2 / nn
+    if merit <= 1:
+        # this code doesn't even beat the surface code, so we don't care about it
+        continue
+
+    # add a summary of this code to the appropriate group of data
+    code = (ll, mm, gg, hh, ii, jj, nn, kk, dd, comm, merit)
+    for cutoff, data in zip(comm_cutoffs, data_groups):
+        if comm <= cutoff:
+            data.append(code)
+            break
 
 ##################################################
 
 os.makedirs(save_dir, exist_ok=True)
 header = "\n".join(headers)
 
-# save groups
-comm_cutoffs = [0] + list(comm_cutoffs)
-for last_comm, comm, data in zip(comm_cutoffs, comm_cutoffs[1:], data_groups):
+# save data groups to files
+for last_comm, comm, data in zip([0] + comm_cutoffs, comm_cutoffs, data_groups):
     file = f"codes_D{last_comm}-{comm}.csv"
     path = os.path.join(save_dir, file)
     np.savetxt(path, data, header=header, fmt=fmt)
