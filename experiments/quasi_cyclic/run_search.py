acd21e56396c3f6803e182e5e718094a05965cde
Michael A. Perlin <mika.perlin@gmail.com>
2024-04-10 20:38:59 -0500

    more frequent updates in quasi-cyclic code search

--- experiments/quasi_cyclic/run_search.py
+++ experiments/quasi_cyclic/run_search.py
@@ -79,8 +79,8 @@ def run_and_save(
     silent: bool = False,
 ) -> None:
     """Compute and save quasi-cyclic code parameters."""
-    if not silent and exponents == (0, 0, 0, 0):
-        print(dims)
+    if not silent and not any(exponents[1:]):
+        print(dims, exponents)
 
     params = get_quasi_cyclic_code_params(dims, exponents, num_trials)
     if params is not None:
