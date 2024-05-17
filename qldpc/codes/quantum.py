4f67b73ae44cdf4f01a926fb04641443465df1bd
Michael A. Perlin <mika.perlin@gmail.com>
2024-05-17 09:41:02 -0500

    one more nit

--- qldpc/codes/quantum.py
+++ qldpc/codes/quantum.py
@@ -709,7 +709,7 @@ class LPCode(CSSCode):
         two-block group-algebra code).  If the base group of the protographs is a cyclic group, the
         resulting lifted product code is a generalized bicycle code.
     - A lifted product code with protographs whose entries get lifted to 1×1 matrices is a
-        hypergraph product code of the lifted protographs.
+        hypergraph product code built from the lifted protographs.
     - One way to get an LPCode: take a classical code with parity check matrix H and multiply it by
         a diagonal matrix D = diag(a_1, a_2, ... a_n), where all {a_j} are elements of a group
         algebra.  The protograph P = H @ D can then be used for one of the protographs of an LPCode.
