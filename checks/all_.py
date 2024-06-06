c95f8159c5f224492170600786f09c4d80dd73c3
Michael A. Perlin <mika.perlin@gmail.com>
2024-06-06 09:38:04 -0500

    initial docs files

--- checks/all_.py
+++ checks/all_.py
@@ -4,5 +4,5 @@ import sys
 import checks_superstaq
 
 if __name__ == "__main__":
-    skip_args = ["--skip", "requirements", "build_docs", "--"]
+    skip_args = ["--skip", "requirements", "--"]
     exit(checks_superstaq.all_.run(*skip_args, *sys.argv[1:]))
