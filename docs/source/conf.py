0a03c613c39920a8394ceba2a362aeece71f8da7
Michael A. Perlin <mika.perlin@gmail.com>
2024-06-06 10:32:30 -0500

    fix module weirdness

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../qldpc"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
