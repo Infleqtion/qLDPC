a67f671568158e434ec1723a36a0735f275058e1
Michael A. Perlin <mika.perlin@gmail.com>
2024-06-07 16:13:38 -0500

    support different gate orders for syndrome extraction

--- qldpc/circuits.py
+++ qldpc/circuits.py
@@ -15,6 +15,8 @@
    limitations under the License.
 """
 
+from collections.abc import Sequence
+
 import numpy as np
 import stim
 
@@ -22,9 +24,28 @@ from qldpc import codes, objects
 
 
 def get_syndrome_extraction_circuit(
-    code: codes.CSSCode, stabilizer_pauli: objects.PauliXZ, error_rate: float, rounds: int = 1
+    code: codes.CSSCode,
+    stabilizer_pauli: objects.PauliXZ,
+    error_rate: float,
+    rounds: int = 1,
+    gate_order: Sequence[tuple[int, int]] | None = None,
 ) -> stim.Circuit:
-    """Get the syndrome extraction circuit for a CSS qubit code."""
+    """Get the syndrome extraction circuit for a CSS qubit code.
+
+    Args:
+        code: the CSS code in question
+        stabilizer_pauli: the Pauli type of the stabilizers to measure
+        error_rate: the probability of SPAM and two-qubit gate errors
+        rounds: the number of syndrome extraction rounds
+        gate_order (optional): the order in which to apply syndrome extraction gates
+    Returns:
+        a stim circuit
+
+    The gate_order is a sequence of tuples (ancilla_qubit_index, data_qubit_index), where the
+    ancilla_qubit_index must be in range(code.num_checks), and the data_qubit_index must be in
+    range(code.num_qubits).  There should be exactly one tuple for each edge in the Tanner graph of
+    the code.
+    """
     if not code.field.order == 2:
         raise ValueError("Syndrome extraction circuits only supported for qubit CSS codes.")
 
@@ -33,6 +54,27 @@ def get_syndrome_extraction_circuit(
     ancillas_x = [len(data_qubits) + qq for qq in range(code.num_checks_x)]
     ancillas_z = [len(data_qubits) + len(ancillas_x) + qq for qq in range(code.num_checks_z)]
     ancillas_xz = ancillas_x + ancillas_z
+
+    if gate_order is None:
+        gate_order = [
+            (ancilla, data_qubit)
+            for ancillas, matrix in [(ancillas_x, code.matrix_x), (ancillas_z, code.matrix_z)]
+            for ancilla, row in zip(ancillas, matrix)
+            for data_qubit in np.nonzero(row)[0]
+        ]
+    else:
+        # assert that there are the correct number of gates addressing the correct qubits
+        num_gates_x = len(np.nonzero(code.matrix_x)[0])
+        num_gates_z = len(np.nonzero(code.matrix_z)[0])
+        assert len(gate_order) == num_gates_x + num_gates_z
+        assert all(
+            (
+                code.matrix_x[ancilla, data_qubit]
+                if ancilla < code.num_checks_x
+                else code.matrix_z[ancilla, data_qubit]
+            )
+            for ancilla, data_qubit in gate_order
+        )
 
     # initialize data qubits
     circuit = stim.Circuit()
@@ -46,14 +88,10 @@ def get_syndrome_extraction_circuit(
     single_round_circuit = stim.Circuit()
 
     single_round_circuit.append("Z_ERROR", ancillas_xz, error_rate)
-    for ancillas, matrix, pauli in [
-        (ancillas_x, code.matrix_x, "X"),
-        (ancillas_z, code.matrix_z, "Z"),
-    ]:
-        for ancilla, row in zip(ancillas, matrix):
-            for data_qubit in np.nonzero(row)[0]:
-                single_round_circuit.append(f"C{pauli}", [ancilla, data_qubit])
-                single_round_circuit.append("DEPOLARIZE2", [ancilla, data_qubit], error_rate)
+    for ancilla, data_qubit in gate_order:
+        gate = "CX" if ancilla < len(ancillas_x) else "CZ"
+        single_round_circuit.append(gate, [ancilla, data_qubit])
+        single_round_circuit.append("DEPOLARIZE2", [ancilla, data_qubit], error_rate)
 
     # noisy syndrome measurement + ancilla reset
     single_round_circuit.append("Z_ERROR", ancillas_xz, error_rate)
