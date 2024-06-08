bf6dacb10e794a65bfb478e7d3db40ffc8eac846
Michael A. Perlin <mika.perlin@gmail.com>
2024-06-08 11:04:22 -0500

    update pytest/coverage exclusions

--- checks/pytest_.py
+++ checks/pytest_.py
@@ -3,6 +3,7 @@ import sys
 
 import checks_superstaq
 
+EXCLUDE = ("checks/*.py", "experiments/*.py", "*/__init__.py", "docs/source/conf.py")
+
 if __name__ == "__main__":
-    exclude = ["checks/*.py", "experiments/*.py", "*/__init__.py"]
-    exit(checks_superstaq.pytest_.run(*sys.argv[1:], exclude=exclude))
+    exit(checks_superstaq.pytest_.run(*sys.argv[1:], exclude=EXCLUDE))
