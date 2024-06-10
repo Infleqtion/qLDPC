96e3a41c0c41ff33415a267576174e09ad2cbb4b
Michael A. Perlin <mika.perlin@gmail.com>
2024-06-10 18:26:19 -0500

    use modular coverage from checks-superstaq

--- checks/coverage_.py
+++ checks/coverage_.py
@@ -1,35 +1,9 @@
 #!/usr/bin/env python3
 import sys
-from collections.abc import Iterable
 
 import checks_superstaq
 import pytest_
 
 
-def run_modular(exclude: str | Iterable[str] = ("*_test.py",) + pytest_.EXCLUDE) -> int:
-    """Check that each file is covered by its own data file."""
-
-    # start by identifying files that should be covered
-    tracked_files = checks_superstaq.check_utils.get_tracked_files("*.py")
-    coverage_files = checks_superstaq.check_utils.exclude_files(tracked_files, exclude)
-
-    # run checks on individual files
-    exit_codes = {}
-    for file in coverage_files:
-        exit_codes[file] = checks_superstaq.coverage_.run(file)
-
-    # print warnings for files that are not covered
-    for file, exit_code in exit_codes.items():
-        if exit_code:
-            checks_superstaq.check_utils.warning(f"Coverage failed for {file}.")
-
-    return sum(exit_codes.values())
-
-
 if __name__ == "__main__":
-    if sys.argv[1:]:
-        # check coverage for the provided arguments
-        exit(checks_superstaq.coverage_.run(*sys.argv[1:]))
-
-    # check for modular coverage
-    exit(run_modular())
+    exit(checks_superstaq.coverage_.run(*sys.argv[1:], "--modular", exclude=pytest_.EXCLUDE))
