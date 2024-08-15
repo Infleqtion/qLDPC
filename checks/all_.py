2e0ce41972c47934d445fe60a678df8805c7e70b
Michael A. Perlin <michael.perlin@jpmchase.com>
2024-08-15 17:42:22 -0500

    remove checks change

--- checks/all_.py
+++ checks/all_.py
@@ -4,5 +4,5 @@ import sys
 import checks_superstaq
 
 if __name__ == "__main__":
-    skip_args = ["--ruff", "--skip", "requirements", "build_docs", "--"]
+    skip_args = ["--ruff", "--skip", "requirements", "--"]
     exit(checks_superstaq.all_.run(*skip_args, *sys.argv[1:], sphinx_paths=["../qldpc"]))
