cff0f8277da532dcbea22bdafe0f52986c1137bf
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-01-08 16:41:23 -0500

    nit

--- qldpc/decoder.py
+++ qldpc/decoder.py
@@ -55,7 +55,7 @@ def decode(
 
 def get_decoder(matrix: npt.NDArray[np.int_], **decoder_args: object) -> Decoder:
     """Retrieve a decoder."""
-    if callable(custom_decoder := decoder_args.pop("decoder", None)):
+    if (custom_decoder := decoder_args.pop("decoder", None)) and callable(custom_decoder):
         return custom_decoder(matrix, **decoder_args)
 
     if decoder_args.pop("with_ILP", False):
