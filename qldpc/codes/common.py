1bd336ba952824eeadb16d024162a0d883496c20
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-01-10 12:02:45 -0500

    clean up example notebook and make some typing fixes

--- qldpc/codes/common.py
+++ qldpc/codes/common.py
@@ -548,7 +548,7 @@ class ClassicalCode(AbstractCode):
 
     def get_logical_error_rate_func(
         self, num_samples: int, max_error_rate: float = 0.3, **decoder_args: Any
-    ) -> Callable[[float], tuple[float, float]]:
+    ) -> Callable[[float | Sequence[float]], tuple[float, float]]:
         """Construct a function from physical --> logical error rate in a code capacity model.
 
         In addition to the logical error rate, the constructed function returns an uncertainty
@@ -1688,7 +1688,7 @@ class CSSCode(QuditCode):
         max_error_rate: float = 0.3,
         pauli_bias: Sequence[float] | None = None,
         **decoder_args: Any,
-    ) -> Callable[[float], tuple[float, float]]:
+    ) -> Callable[[float | Sequence[float]], tuple[float, float]]:
         """Construct a function from physical --> logical error rate in a code capacity model.
 
         In addition to the logical error rate, the constructed function returns an uncertainty
