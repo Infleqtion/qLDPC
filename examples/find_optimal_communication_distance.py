2d4824b7ac1d957500443c3b3f73fc293d253924
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-01-19 13:50:02 -0600

    clarifying comment

--- examples/find_optimal_communication_distance.py
+++ examples/find_optimal_communication_distance.py
@@ -72,7 +72,7 @@ def get_placement_matrix(code: qldpc.codes.BBCode, folded_layout: bool) -> npt.N
     """
     Vectorized calculation of displacements, with shape = (len(nodes), len(locs), num_neighbors, 2).
     Here dispalacements[node_idx, loc_idx, neighbor_idx, :] is the displacement between a given node
-    and a given neighbor when placed at a given location.
+    and a given neighbor when the node is placed at a given location.
     """
     displacements = (
         np.array(locs, dtype=int)[None, :, None, :]
