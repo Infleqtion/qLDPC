9ac90d206c21014c2fff6dab27bc92f563a15732
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-01-22 11:15:26 -0600

    fix sector bug

--- examples/find_optimal_communication_distance.py
+++ examples/find_optimal_communication_distance.py
@@ -191,18 +191,19 @@ def get_qubit_pos_func(
         """Get the default position of the given qubit/node."""
         plaquette_index = qubit_index % num_plaquettes
         if qubit_index < num_plaquettes:
-            sector = "X" if is_data else "L"
+            sector = "L" if is_data else "X"
+            aa, bb = plaquette_map_l[
+                plaquette_index // code.orders[1],
+                plaquette_index % code.orders[1],
+            ]
             orders = orders_l
-            aa, bb = plaquette_index // code.orders[1], plaquette_index % code.orders[1]
-            aa, bb = plaquette_map_l[aa, bb]
-
         else:
-            sector = "Z" if is_data else "R"
+            sector = "R" if is_data else "Z"
+            aa, bb = plaquette_map_r[
+                (plaquette_index // code.orders[1] + shift_r[0]) % code.orders[0],
+                (plaquette_index % code.orders[1] + shift_r[1]) % code.orders[1],
+            ]
             orders = orders_r
-            aa = (plaquette_index // orders[1] + shift_r[0]) % code.orders[0]
-            bb = (plaquette_index % orders[1] + shift_r[1]) % code.orders[1]
-            aa, bb = plaquette_map_r[aa, bb]
-
         return code.get_qubit_pos((sector, aa, bb), folded_layout, orders=orders)
 
     return get_qubit_pos
