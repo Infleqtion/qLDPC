273c5ed61bf087ba7dc656020603c1f0bd17238e
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-01-24 13:47:23 -0500

    remove unnecessary indexing

--- examples/find_bbcode_layouts.py
+++ examples/find_bbcode_layouts.py
@@ -141,7 +141,7 @@ def get_max_distance(code: qldpc.codes.BBCode) -> float:
 def get_check_supports(code: qldpc.codes.BBCode) -> npt.NDArray[np.int_]:
     """Identify the support of the parity checks of the given code."""
     return np.vstack(
-        [np.where(stabilizer)[0] for stabilizer in itertools.chain(code.matrix_z, code.matrix_x)]
+        [np.where(stabilizer) for stabilizer in itertools.chain(code.matrix_z, code.matrix_x)]
     )
 
 
