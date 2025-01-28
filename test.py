be4f30d5421ef16e380f241edfd04f8ade151b85
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-01-28 09:51:01 -0500

    better test

--- test.py
+++ test.py
@@ -5,23 +5,34 @@ import numpy as np
 import qldpc
 import test
 
-code = qldpc.codes.ToricCode(12)
+code = qldpc.codes.ToricCode(10)
 code._exact_distance_x = None
 code._exact_distance_z = None
+
+
+stabilizers_x = code.code_x.canonicalized().matrix
+logical_ops = code.get_logical_ops()
+
+pauli = qldpc.objects.Pauli.X
+logical_ops_x = logical_ops.reshape(2, -1, 2, len(code))[pauli, :, pauli, :]
+
+
+stabilizers_x = stabilizers_x.view(np.ndarray)
+logical_ops_x = logical_ops_x.view(np.ndarray)
 
 start = time.time()
 
 print()
-for stabilizers, pauli in [
-    (code.matrix_x, qldpc.objects.Pauli.X),
-    (code.matrix_z, qldpc.objects.Pauli.Z),
-]:
-    stabilizers = stabilizers.row_reduce()
-    logical_ops = code.get_logical_ops(pauli).reshape(-1, 2, len(code))[:, pauli, :]
-    dist = test.get_css_sector_distance(stabilizers.view(np.ndarray), logical_ops.view(np.ndarray))
-    print(pauli, dist)
-
+dist = test.get_css_sector_distance(stabilizers_x, logical_ops_x)
+print(dist)
 print(time.time() - start)
+
+
+print()
+dist = test._get_css_sector_distance_large(stabilizers_x, logical_ops_x)
+print(dist)
+print(time.time() - start)
+
 
 # start = time.time()
 # for _ in range(1):
