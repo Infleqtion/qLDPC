1918b0c5f38170d17e9a59280890930c93e2b7d6
Victory Omole <vtomole2@gmail.com>
2025-01-29 14:14:01 -0600

    Fix return type of `get_encoding_tableau` (#209)
    * Update circuits.py
    
    * Update circuits.py

--- qldpc/circuits.py
+++ qldpc/circuits.py
@@ -41,7 +41,7 @@ def restrict_to_qubits(func: Callable[..., stim.Circuit]) -> Callable[..., stim.
 
 
 @restrict_to_qubits
-def get_encoding_tableau(code: codes.QuditCode) -> stim.Circuit:
+def get_encoding_tableau(code: codes.QuditCode) -> stim.Tableau:
     """Tableau to encode physical states at its input into logical states of the given code.
 
     For all j in {0, 1, ..., code.dimension - 1}, this tableau maps weight-one X_j and Z_j operators
@@ -85,7 +85,7 @@ def get_encoding_tableau(code: codes.QuditCode) -> stim.Circuit:
 
 
 @restrict_to_qubits
-def get_encoding_circuit(code: codes.QuditCode) -> stim.Tableau:
+def get_encoding_circuit(code: codes.QuditCode) -> stim.Circuit:
     """Circuit to encode physical states at its input into logical states of the given code.
 
     For all j in {0, 1, ..., code.dimension - 1}, this circuit maps weight-one X_j and Z_j operators
