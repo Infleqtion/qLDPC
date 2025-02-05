a470e106f35352669874d37b8ef8b83fc9c1b52e
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-02-05 10:26:10 -0500

    fix coverage

--- qldpc/decoders_test.py
+++ qldpc/decoders_test.py
@@ -20,6 +20,7 @@ from __future__ import annotations
 import numpy as np
 import numpy.typing as npt
 import pytest
+import scipy
 
 from qldpc import decoders
 
@@ -45,19 +46,25 @@ def test_custom_decoder() -> None:
 def test_decoding() -> None:
     """Decode a simple problem."""
     matrix = np.eye(3, 2, dtype=int)
-    syndrome = np.array([1, 1, 0])
     error = np.array([1, 1], dtype=int)
+    syndrome = np.array([1, 1, 0], dtype=int)
     assert np.allclose(error, decoders.decode(matrix, syndrome, with_ILP=True))
     assert np.allclose(error, decoders.decode(matrix, syndrome, with_MWPM=True))
     assert np.allclose(error, decoders.decode(matrix, syndrome, with_BF=True))
-    assert np.allclose(error, decoders.decode(matrix, syndrome, with_BP_OSD=True))
     assert np.allclose(error, decoders.decode(matrix, syndrome, with_BP_LSD=True))
+    assert np.allclose(error, decoders.decode(matrix, syndrome, with_BP_OSD=True))
 
     # the distance of the given code is undefined, so lookup decoding to half the distance fails
     assert np.allclose([0, 0], decoders.decode(matrix, syndrome, with_lookup=True))
 
     # but it works if we manually tell it to try and decode errors of weight <= 2
     assert np.allclose(error, decoders.decode(matrix, syndrome, with_lookup=True, max_weight=2))
+
+    # decode two copies of the problem
+    block_error = np.concatenate([error, error])
+    block_syndrome = np.concatenate([syndrome, syndrome])
+    block_decoder = decoders.BlockDecoder(syndrome.size, decoders.get_decoder(matrix))
+    assert np.allclose(block_error, block_decoder.decode(block_syndrome))
 
     # decode over trinary field
     modulus = 3
