7ceae67bffcdeb313c1675fde89f4d752117372f
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-02-26 16:09:10 -0500

    glob

--- build-cython.py
+++ build-cython.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import distutils.ccompiler
+import glob
 import os
 import shutil
 import sys
@@ -13,7 +14,7 @@ from setuptools.command.build_ext import build_ext
 def build_cython(*extra_compile_args: str, rebuild: bool = False) -> None:
     extension = Extension(
         "*",
-        ["*/**/*.pyx"],
+        glob.glob("**/*.pyx", recursive=True),
         include_dirs=[numpy.get_include()],
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
         extra_compile_args=list(extra_compile_args),
