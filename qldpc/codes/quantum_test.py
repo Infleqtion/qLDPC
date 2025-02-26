df0771d3c91fc531828518a1d2d5a935e78f552f
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-02-26 13:43:47 -0500

    typo

--- qldpc/codes/quantum_test.py
+++ qldpc/codes/quantum_test.py
@@ -241,7 +241,7 @@ def test_subsystem_hypergraph_product(
     code_b = codes.ClassicalCode.random(*bits_checks_b, field=field)
     code = codes.SHPCode(code_a, code_b)
 
-    # assert validityp of the the "natural" stabilizers that are set at initialization
+    # assert validity of the the "natural" stabilizers that are set at initialization
     stabilizer_ops = code.get_stabilizer_ops()
     assert np.array_equal(
         stabilizer_ops.row_reduce(),
