3aefef82fb3bf2c241a06dcabf2db520693b8a6a
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-03-05 11:00:40 -0500

    fix checks/all_.py

--- checks/all_.py
+++ checks/all_.py
@@ -4,5 +4,5 @@ import sys
 import checks_superstaq
 
 if __name__ == "__main__":
-    skip_args = ["--ruff", "--skip", "configs", "requirements", "build_docs", "--"]
-    exit(checks_superstaq.all_.run(*skip_args, *sys.argv[1:]))
+    skip_and_ruff = ["--skip", "configs", "requirements", "build_docs", "--ruff"]
+    exit(checks_superstaq.all_.run(*skip_and_ruff, *sys.argv[1:]))
