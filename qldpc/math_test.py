4b54fdd05f6efa541533f4e4ef8128e92582890c
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-03-06 21:53:16 -0500

    fix coverage

--- qldpc/math_test.py
+++ qldpc/math_test.py
@@ -27,6 +27,8 @@ def test_vectors() -> None:
     vectors = np.array([[0, 1], [1, 2]], dtype=int)
     vectors_conj = np.array([[-1, 0], [-2, 1]], dtype=int)
     assert np.array_equal(qldpc.math.symplectic_conjugate(vectors), vectors_conj)
+
+    assert np.array_equal(qldpc.math.first_nonzero_cols(np.empty(0)), [])
     assert np.array_equal(qldpc.math.first_nonzero_cols(vectors), [1, 0])
     assert np.array_equal(qldpc.math.first_nonzero_cols(vectors_conj), [0, 0])
 
