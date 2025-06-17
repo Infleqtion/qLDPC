47908453c0960bdcb1e258a5949ce97cf2642307
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-06-17 16:03:29 -0400

    clean up example

--- qldpc/codes/quantum.py
+++ qldpc/codes/quantum.py
@@ -1026,19 +1026,22 @@ class SLPCode(CSSCode):
     As an example, the SLPCode in examples 1 on page 6 of https://arxiv.org/pdf/2404.18302v1 can be
     constructed by
 
-        group = qldpc.abstract.CyclicGroup(2)
-        ring = qldpc.abstract.GroupRing(group)
-        x = group.generators[0]
-        matrix = qldpc.abstract.RingArray.build([[1, x, x], [x, x, 1]], ring)
-        code = qldpc.codes.SLPCode(matrix)
+        from qldpc.abstract import CyclicGroup, GroupRing, RingMember, RingArray
+        from qldpc.codes import SLPCode
+
+        group = CyclicGroup(2)
+        ring = GroupRing(group)
+        x = RingMember(ring, group.generators[0])
+        matrix = RingArray([[ring.one, x, x], [x, x, ring.one]])
+        code = SLPCode(matrix)
 
     while the SLPCode in example 2 is similarly
 
-        group = qldpc.abstract.CyclicGroup(3)
-        ring = qldpc.abstract.GroupRing(group)
-        x = qldpc.abstract.RingMember(ring, group.generators[0])
-        matrix = qldpc.abstract.RingArray([[ring.one + x + x**2, ring.one + x, x]])
-        code = qldpc.codes.SLPCode(matrix)
+        group = CyclicGroup(3)
+        ring = GroupRing(group)
+        x = RingMember(ring, group.generators[0])
+        matrix = RingArray([[ring.one + x + x**2, ring.one + x, x]])
+        code = SLPCode(matrix)
 
     Note: for some reason, the code parameters obtained above with code.get_code_params() differ
     from those reported in arXiv:2404.18302v1; we should figure out why.
